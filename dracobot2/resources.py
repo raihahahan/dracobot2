42544dc3596d98cf44b1d62fb163176a3bf91805
raihahahan <mraihandev@gmail.com>
2023-10-14 17:29:16 +0800

    edited bot messages

--- dracobot2/resources.py
+++ dracobot2/resources.py
@@ -54,9 +54,9 @@ DONE_KEY = u"done"
 def comma_separate(str_list: list):
     return ", ".join(str_list)
 
-CURRENT_MAINTAINERS = "Raihan and Wonje"
-PAST_MAINTAINERS = ["Daniel Lau", "Ji Cheng", "Shao Yi", "Bai Chuan", "Fiz, Youkuan", "Kang Ming", "Zhi Yu"]
-TECHNICAL_ASSISTANCE = "@raihahan or @jeonwonje"
+CURRENT_MAINTAINERS = "Raihan & Wonje"
+PAST_MAINTAINERS = ["Ji Cheng", "Shao Yi", "Bai Chuan", "Fiz, Youkuan", "Kang Ming", "Zhi Yu", "Javier", "Raihan", "Wonje"]
+TECHNICAL_ASSISTANCE = "@xavierchia"
 REACH_OUT_TO = "@notbingsu, @yukuleles, @xavierchia, @vitchun"
 
 
@@ -64,11 +64,14 @@ REACH_OUT_TO = "@notbingsu, @yukuleles, @xavierchia, @vitchun"
 ABOUT_THE_BOT = DRAGON_EMOJI + " *About DracoBot* " + DRAGON_EMOJI + "\n\n" + CAKE_EMOJI + " Birthday: June 2017\n\n" +\
     ROBOT_EMOJI + f" Currently maintained by {CURRENT_MAINTAINERS}\n\n" + SKULL_EMOJI +\
     f" Past Bot Developers: {comma_separate(PAST_MAINTAINERS)}\n\n"
+
 ADMIN_GREETING = "Hello there, Administrator! What do you want to say to everyone?\n" +\
     "Whatever you submit from now on will be broadcasted to all users, be CAREFUL!\n" +\
     "Type /" + DONE_KEY + " to exit, once you have made your announcement."
+
 HELLO_GREETING = "Hello there, {}! DracoBot at your service! Press /" + \
     MENU_KEY + " to bring up keyboard! " + DRAGON_EMOJI
+
 HELP_MESSAGE = "Hello there, {}!\n\n" +\
     "<u>Main Menu</u>\n" +\
     DRAGON_CHAT_KEY + ": To chat with your Dragon \n" +\
@@ -86,58 +89,65 @@ HELP_MESSAGE = "Hello there, {}!\n\n" +\
     "4. <b>Media files</b>: Supported files are <i>audio</i>, <i>document</i>, <i><b>photo</b></i>, <i><b>sticker</b></i>, <i>videos</i>, <i>video note</i> and <i>voice</i>\n\n" +\
     f"Please message {TECHNICAL_ASSISTANCE} if you need technical assistance!\n" +\
     "Thank you and we hope you'll have fun throughout this game! :)"
+
 GAME_RULES_MESSAGE = "Rules of Dragon and Trainer " + DRAGON_EMOJI + "\n\n" +\
     "Each of you who participated will be assigned a Trainer and a Dragon. " +\
     "Of course, you will know the identity of your Dragon while your Trainer’s identity will be kept " +\
     "from you. Throughout the course of the event, feel free to chat with both your Dragon and Trainer " +\
     "through this telegram bot where your identity will be kept secret, and take care of your dragon with " +\
-    "anonymous gifts " + GIFT_EMOJI + " and pranks " + DEVIL_EMOJI + " according to their indicated tolerance levels! " +\
+    "anonymous gifts " + GIFT_EMOJI + " and pranks " + DEVIL_EMOJI + " according to their indicated commitment levels! " +\
     "Of course, you can look forward to seeing what your own trainer does for you as well! " + WINK_EMOJI + GRIN_EMOJI + SPARKLE_EMOJI + "\n\n" +\
-    "<u>Explanation for tolerance levels</u>\n\n" +\
-    "1: Gift Exchange, do nice things only!\n" +\
-    "2: Pranks are to be minimal, and no / minimal clean up required!\n" +\
-    "3: Pranks are fine, but do take care of what your dragon says is <b>OFF LIMITS</b>\n\n" +\
-    "<u>Dos</u> " + SMILE_EMOJI + "\n" +\
-    "• Observe the Tolerance Levels your dragons have indicated.\n" +\
-    f"• Gain consent from your dragon before entering their rooms. (You can ask using the @{BOT_NAME} via the chat function)\n" +\
-    "• Do take note of your dragon’s dislikes and OFF LIMITS details and adhere to them (Try to <b>avoid</b> doing anything your dragon dislikes, especially anything they have declared OFF LIMITS)\n" +\
-    "• <b>Balance out the pranks with gifts - moderation is key!</b>\n" +\
-    "• Try (your best) to <b>keep your identity hidden</b>.\n" +\
-    "• Be active in the event! :)\n" +\
-    "• " + SPARKLE_EMOJI + "<b>Share your pranks and gifts throughout the event in the Draco group chat!</b>" + SPARKLE_EMOJI + "\n\n" +\
-    "<u>Don'ts</u> " + SAD_EMOJI + CROSS_EMOJI + "\n" +\
-    "• Cause major damage (e.g. breaking a treasured object) even if they’ve indicated no boundaries.\n" +\
-    "• Flout other RC / NUS rules (e.g. theft, possession of alcohol *ahem ahem*).\n" +\
-    "• Cause major inconveniences, especially along the common corridor" +\
-    "(e.g. blockade the walkway, pranks involving powdered substances like flour or curry powder).\n" +\
-    "• Cause fire hazards and hinder evacuation routes.\n" +\
-    "• Write, draw or scribble any obscene/vulgar contents on doors / common area.\n" +\
-    "• Commit pranks that may put <b>YOURSELF</b> or <b>OTHERS AT RISK</b> " + SWORD_EMOJI + KNIFE_EMOJI + FIRE_EMOJI + " (e.g. placing personal objects at dangerous locations, using flammables)\n\n" +\
-    "<b>**IMPORTANT!**</b>\n\n" +\
-    "NO LIVE ANIMALS OR INSECTS\n" +\
-    "NO MOVING OF FURNITURE OUT OF THE ROOMS and AVOID MOVING of furniture\n" +\
-    "And RESTRICT pranks to <b>YOUR OWN DRAGON</b> (avoid performing pranks on others’ dragons as any incidents that arise due to your unannounced pranks would implicate other trainers)\n\n" +\
-    "Do adhere to the rules as stated above, as well as the basic housing regulations of RC4.\n\n" +\
-    f"If you have any other questions, concerns or doubts, don’t be afraid to reach out to {REACH_OUT_TO}! We hope you have fun and make new friends as well!\n\n" +\
+    "<u>Explanation for commitment levels</u>\n\n" +\
+    "Level 1: No pranks, No entering participant’s rooms, Light gift giving\n" +\
+    "Level 2: Light pranks, Entering participants room upon strict consent with restrictions, Gift Giving proportionate to Prank\n" +\
+    "Level 3: Highest Level of Tolerance, Room Entering is allowed (subject to personal preference), Gift Giving proportionate to Prank\n\n" +\
+    "Throughout the course of the event, ensure your actions are within the Housing Services Code of Conduct and Rules. Participants will be held accountable for any damages caused and will face the appropriate disciplinary action. As part of our efforts to create a safe but fun environment for everyone, please read the following rules beforehand:\n\n" +\
+    f"1) <b>DO NOT ENTER</b> your dragon’s room <b>UNTIL CONSENT IS Given</b> {GREEN_STATUS_EMOJI}\nPlease note the consent details, and clarify with your dragon if you are ever in doubt.\n\n" +\
+    f"2) <b>TAKE NOTE OF FOOD ALLERGIES</b> {CAKE_EMOJI}. Please check with your dragon on his/her allergies before giving any food/gifts. Please tell your trainer your allergies if you have any.\n\n" +\
+    f"3) <b>ADHERE</b> to what your dragon dislikes. Please avoid doing anything your dragon dislikes, especially the things they have declared <b>OFF Limits</b> {CROSS_EMOJI}\n\n" +\
+    f"4) <b>RESTRICT</b> surprises to your <b>OWN DRAGON</b>. Avoid performing surprises onto other people apart from your dragon as any incidents that happen due to your unannounced surprises could implicate other trainers.\n\n" +\
+    f"5) <b>ADHERE TO THE INDICATED</b> Commitment Level {SPARKLE_EMOJI}. Do not do anything outside the indicated commitment level. If you and your dragon are at level 1, please restrict to gift giving only. The same is true for level 2 and 3.\n\n" +\
+    f"6) <b>BALANCE</b> out the surprises with gifts - <b>MODERATION IS KEY</b>!\n\n" +\
+    f"7) <b>AVOID REMOVAL or MOVING OF ITEMS/FURNITURE</b> {SKULL_EMOJI}. Any missing items reported from your dragon's room could implicate you. DAMAGED furniture could subject you to disciplinary action. <b>FURNITURE IS NOT TO BE MOVED OUT OF ROOMS and DO NOT REMOVE ANY DOORS</b>.\n\n" +\
+    f"8) NO ANIMALS OR INSECTS {WHALE_EMOJI} (Dead or alive). Housing Services does not tolerate the use of live animals or insects as it contravenes housing regulations and could put other residents at risk of Injury. <b>FAILURE TO COMPLY COULD RESULT IN DEMERIT POINTS</b>.\n\n" +\
+    "9) <b>DO NOT</b> commit surprises that <b>MAY PUT YOURSELF OR OTHERS AT RISK</b>. This includes but is not restricted to:\n" +\
+    "> Placing personal objects at dangerous locations\n" +\
+    "> Entering toilets of the opposite gender\n" +\
+    "> Using flammables/creating an open flame\n" +\
+    "> Entering rooms using the windows\n" +\
+    "> Placing heavy objects in high places\n" +\
+    "> Blocking fire exits and common corridors\n\n" +\
+    "10) Non-Exhaustive List of Banned surprises:\n" +\
+    "> Powder-related surprises\n" +\
+    "> Surprises that extend past Draco floors\n" +\
+    "> Spoilage of food or surprises relating to smell\n" +\
+    "> Surprises involving different gendered toilets \n\n" +\
+    f"If you have any other questions, concerns or doubts, don’t be afraid to reach out to {REACH_OUT_TO}!\n\n We hope you have fun and make new friends as well!\n\n" +\
     "Love,\n" +\
-    "Draco House Comm" + BLUE_HEART_EMOJI
+    "Dragon & Trainer Organising Committee" + BLUE_HEART_EMOJI + DRAGON_EMOJI
+
 WELCOME_MESSAGE = "Dear {name},\n\n\n"\
-    "You woke up dizzy in the highest floors of a building known as AreSeaFore and you are unsure of how you were transported here. Next to you lies a piece of paper which reads:\n\n\n"\
-    "\"Esteemed Trainer, you are tasked to train dragon ‘{dragon_name}’ for the next 2 and a half weeks. Slowly approach the dragon at unit {dragon_room_number} to tame it and teach it new skills.\n\n"\
-    "Your dragon likes ‘{dragon_likes}’ but it really dislikes ‘{dragon_dislikes}‘.\n\n"\
-    "‘{dragon_requests}' is stated as off limits. Take careful note of these as you venture on your quest to tame your dragon, for there may be consequences.\n\nThe difficulty of training is LEVEL {dragon_level:d}.\n\n\n"\
+    "Welcome to the Land Above All, where our spirits intertwine and legends come to life. You have been sent on a mission atop RC4, tasked with training your dragon companion, ‘{dragon_name}‘. Over the next two weeks, forge a bond, tame the wild, and explore the depths of companionship. Embrace this extraordinary journey and uncover the hidden potentials within both you and your dragon.\n\n\n"\
+    "You slowly approach your Dragon's lair at {dragon_room_number}. At the entrance of the lair is a signage which reads: \n\n"\
+    "{dragon_name} is a Commitment Level {dragon_level:d} dragon.\n"\
+    "{dragon_name}'s likes include \"{dragon_likes}\".\n"\
+    "{dragon_name}'s dislikes and no-gos include \"{dragon_dislikes}\".\n"\
+    "{dragon_name}'s consent details: {dragon_requests}\n\n"\
+    "Take careful note of these as you venture on your quest to tame your dragon, for there may be consequences.\n\n"\
     f"We, {REACH_OUT_TO}, will watch over all of you trainers throughout the training. Do contact us should you need assistance in taming your dragon.\n\n\n"\
-    "Set forth young one and be the best dragon tamer of AreSeaFore Draco.\"\n\n\n"\
-    "*Game of Thrones Music Intensifies*"
+    "Set forth young one and be the best dragon tamer in the Land Above All.\n\n\n"
+
 STATUS = "Trainer Status: {trainer_status}\n"\
     "Dragon Status: {dragon_status}\n"
+
 DRAGON_DETAILS = "Dragon Details " + DRAGON_EMOJI + "\n\n"\
     "Name: {name}\n"\
     "Unit: {room_number}\n"\
-    "Level: {level:d}\n\n"\
+    "Commitment Level: {level:d}\n\n"\
     "Likes:\n{likes}\n\n"\
     "Dislikes:\n{dislikes}\n\n"\
-    "Off Limits:\n{requests}\n\n"
+    "Consent Details:\n{requests}\n\n"
+
 TRAINER_DETAILS = "Trainer Details " + TRAINER_EMOJI + SPARKLE_EMOJI + "\n\n"\
     "Name: {name}\n"\
     "Unit: {room_number}\n"
